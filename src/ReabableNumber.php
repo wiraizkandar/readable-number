9d7aa51277c4a80240aab86171bc5b0cd83bba90
wiraizkandar <wira.izkandar@involve.asia>
2022-11-17 17:05:22 +0800

    change min number range

--- src/ReabableNumber.php
+++ src/ReabableNumber.php
@@ -10,7 +10,7 @@ class ReadableNumber
 	 */
 	public static function numberForHumans(int $number, int $maxDecimal = 1): string
 	{
-		if ($number > 1000 && $number < 1000000) {
+		if ($number >= 1000 && $number < 1000000) {
 			return sprintf('%d%s', round($number / 1000, $maxDecimal), 'K');
 		}
 
